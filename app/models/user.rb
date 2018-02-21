667c9ed6043f03126e0ba648a07d12867096ee92
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-21 17:50:07 +0800

    prepared the routes and worked on the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :bikes
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :bikes do
+    resources :reviews, except: [:index, :destroy]
+    resources :bookings
+  end
+
+  resources :users, only: [:show]
+
 end

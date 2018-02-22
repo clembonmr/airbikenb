4342078add6b04b5fcc45a19e7ea9350a80c8e24
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-22 12:06:13 +0800

    worked on the validations and authentification

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :description, :rate, presence: true
+  validates :rate, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }
 end

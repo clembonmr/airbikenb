58fd5e9af1ad2f3af45ed80e75fa13a7f634b5a6
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-22 15:36:57 +0800

    added average_rate method to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,10 @@ class Review < ApplicationRecord
   belongs_to :booking
   validates :description, :rate, presence: true
   validates :rate, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }
+
+  def average_rate
+    @bike = Bike.find(params(:bike_id))
+    Review.average(:rate).where(bike_id: @bike.id)
+  end
+
 end

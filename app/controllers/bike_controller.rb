80a61d89a0066778de917e2f455a77b4f0ff6045
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-22 14:46:38 +0800

    created Bike and Review controllers

---
+++ app/controllers/bike_controller.rb
@@ -0,0 +1,45 @@
+class BikeController < ApplicationController
+before_action :set_bike, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @bikes = Bike.all
+  end
+
+  def show
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    if @bike.save
+      redirect_to bike_path(@bike.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @bike.update(bike_params)
+    redirect_to bikes_path
+  end
+
+  def destroy
+    @bike.destroy
+    redirect_to bikes_path
+  end
+
+  def bike_params
+    params.require(:bike).permit(:brand, :category, :description, :photo, :daily_price, :latitude, :longitude, :user_id, :availability)
+  end
+
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+end
---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,43 @@
+class ReviewController < ApplicationController
+
+  before_action :set_booking
+
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.booking = @booking
+
+    if @review.save
+      redirect_to booking_path(@booking)
+    else
+      render :new  # renders the new view
+    end
+  end
+
+  def edit
+    @review = @booking.review
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to bike_path(@booking.bike)
+  end
+
+  def destroy
+    @review = @booking.review
+    @review.destroy
+    redirect_to bike_path(@booking.bike)
+  end
+
+  def review_params
+    params.require(:review).permit(:description, :rate)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+end

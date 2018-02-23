3b271e6f3d093ae6309f9911240962235a2c0141
guido caldara <guido.caldara@gmail.com>
2018-02-23 17:19:18 +0800

    bike show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :rate, presence: true
   validates :description, :rate, presence: true
   validates :rate, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }
 

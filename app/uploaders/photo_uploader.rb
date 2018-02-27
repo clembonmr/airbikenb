83cb7124179c6971974df583aa9693d84714e8e4
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-27 11:14:02 +0800

    progress on CSS

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,4 +3,12 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  version :card do
+    resize_to_fit 600, 600
+  end
+
+  version :banner do
+    resize_to_fit 1600, 1600
+  end
+
 end

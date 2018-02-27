c0215bc3586410bd7755a273e53dc9eca52b6d5d
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-02-27 15:15:08 +0800

    add stars to cards

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,9 +4,5 @@ class Review < ApplicationRecord
   validates :description, :rate, presence: true
   validates :rate, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }
 
-  def average_rate
-    @bike = Bike.find(params(:bike_id))
-    Review.average(:rate).where(bike_id: @bike.id)
-  end
 
 end
